a1bd7528b99aaadf2fc91b243562e29f718d58a4
Daniel Meißner <daniel@3st.mine.nu>
2012-01-06 01:45:56 +0100

    Added license information.

--- lib/compass.rb
+++ lib/compass.rb
@@ -1,3 +1,24 @@
+# encoding: utf-8
+
+=begin
+Copyright Daniel Meißner <dm@3st.be>, 2012
+
+This file is part of FreifunkKBUInfoPage.
+
+FreifunkKBUInfoPage is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FreifunkKBUInfoPage is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FreifunkKBUInfoPage. If not, see <http://www.gnu.org/licenses/>.
+=end
+
 require 'compass'
 
 Compass.add_project_configuration 'compass_config.rb'
